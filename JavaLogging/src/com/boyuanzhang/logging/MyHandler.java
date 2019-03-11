a6fceb254e16fb433949034e02a1933ce04355c1
alexander.boyuan.zhang <alexander.boyuan.zhang@gmail.com>
2019-03-11 23:11:14 +0100

    no message

---
+++ JavaLogging/src/com/boyuanzhang/logging/MyHandler.java
@@ -0,0 +1,31 @@
+package com.boyuanzhang.logging;
+
+/*
+ * https://www.journaldev.com/977/logger-in-java-logging-example
+ * */
+
+import java.util.logging.LogRecord;
+import java.util.logging.StreamHandler;
+
+public class MyHandler extends StreamHandler {
+
+    @Override
+    public void publish(LogRecord record) {
+        //add own logic to publish
+        super.publish(record);
+    }
+
+
+    @Override
+    public void flush() {
+        super.flush();
+    }
+
+
+    @Override
+    public void close() throws SecurityException {
+        super.close();
+    }
+
+}
+
---
+++ JavaLogging/src/com/boyuanzhang/logging/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Boyuan Zhang
+ * source https://www.journaldev.com/977/logger-in-java-logging-example
+ *
+ */
+package com.boyuanzhang.logging;
